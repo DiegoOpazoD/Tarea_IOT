a34ea0436feb0d619c647c2c84c6c19643545cb1
Matías Jesús Cisterna Nauto <matiasjesus300@gmail.com>
2024-09-27 11:39:54 -0300

    Generar tabla log

--- server/raspi/models.py
+++ server/raspi/models.py
@@ -18,7 +18,18 @@ class BaseModel(Model):
 class Dev(BaseModel):
     device_mac = CharField(unique=True, max_length=12)  # 6 bytes en formato hex (12 caracteres)
 
+# Tabla Log: Llevará un registro de todos los mensajes recibidos
+class Log(BaseModel):
+    packet_id = AutoField()                               # Identificador del paquete, serial auto-incremental
+    fk_device_id = ForeignKeyField(Dev, backref='logs')   # Relacionado con el dispositivo (device_id)
+    
+    # Columnas para la información del header
+    msg_id = IntegerField()                               # Identificador de mensaje (2 bytes)
+    protocol_id = IntegerField()                          # Protocolo (1 byte)
+    transport_layer = IntegerField()                      # Capa de transporte (1 byte)
+    length = IntegerField()                               # Longitud del mensaje (2 bytes)
+
 
 
 # Se crean las tablas
-db.create_tables([Dev])
+db.create_tables([Dev,Log])
